fd70b9a054d2fec5d8c8441a3df16a25a79ac89b
CHhierro <119745798+CHhierro@users.noreply.github.com>
2022-12-03 23:59:27 +0800

    Create List.py
    Signed-off-by: CHhierro <119745798+CHhierro@users.noreply.github.com>

---
+++ List.py
@@ -0,0 +1,12 @@
+umbers = [1,2,3]
+strings = ["hello","world"]
+names = ["Christian", "Mark", "Hierro"]
+
+# write your code here
+second_name = names[1]
+
+
+# this code should write out the filled arrays and the second name in the names list (Mark).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
